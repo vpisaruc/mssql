1758e543a00e66e9a803859f429d37ab444ceda7
vpisaruc <36450321+vpisaruc@users.noreply.github.com>
2018-11-21 15:09:31 +0300

    Update

---
+++ lab3/A/function2.sql
@@ -0,0 +1,24 @@
+-- Подставляемая табличная функция
+IF OBJECT_ID (N'dbo.orderProductInStock', N'FN') IS NOT NULL
+    DROP FUNCTION dbo.orderProductInStock
+GO
+
+CREATE FUNCTION dbo.orderProductInStock	(@transactionId int)
+RETURNS TABLE
+AS
+RETURN (
+    SELECT o.idTransaction, p.id, p.productName,
+	CAST(
+		CASE
+			WHEN p.productInStock = 0 
+				THEN 'Out of stock'
+				ELSE 'In stock'
+		END AS varchar(20)
+		) as inStock
+    FROM tbProduct p JOIN tbOrder o ON p.id = o.idProduct
+    WHERE o.idTransaction = @transactionId
+    )
+GO
+
+SELECT * FROM dbo.orderProductInStock(5)
+GO
