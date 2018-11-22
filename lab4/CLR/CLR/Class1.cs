3867d0e55ef1deef3c39a771441d225a5651f90a
vpisaruc <36450321+vpisaruc@users.noreply.github.com>
2018-11-22 14:05:04 +0300

    update

--- lab4/CLR/CLR/Class1.cs
+++ lab4/CLR/CLR/Class1.cs
@@ -2,6 +2,8 @@
 using System.Data;
 using Microsoft.SqlServer.Server;
 using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Collections;
 
 public class HelloWorldProc
 {
@@ -12,4 +14,43 @@ public class HelloWorldProc
         text = "Hello world!";
     }
 
+}
+
+
+public partial class UserDefinedFunctions
+{
+    [SqlFunctionAttribute()]
+    public static SqlInt32 ScalarFunc1(SqlDateTime beginingDate, SqlDateTime endDate)
+    {
+        ArrayList resultCollection = new ArrayList();
+        using (SqlConnection connection = new SqlConnection("context connection=true"))
+        {
+            connection.Open();
+            using (SqlCommand selectPaymentAmount = new SqlCommand(
+             "SELECT " +
+             "[paymentAmount]" +
+             "FROM [tbTransaction] " +
+             "WHERE \"date\" BETWEEN @beginingDate and @endDate;",
+             connection))
+            {
+                SqlParameter modifiedBeginingDate = selectPaymentAmount.Parameters.Add(
+                    "@beginingDate", SqlDbType.DateTime);
+
+                SqlParameter modifiedEndDate = selectPaymentAmount.Parameters.Add(
+                    "@endDate", SqlDbType.DateTime);
+
+                modifiedBeginingDate.Value = beginingDate;
+                modifiedEndDate.Value = endDate;
+
+                using (SqlDataReader amountReader = selectPaymentAmount.ExecuteReader())
+                {
+                    while (amountReader.Read())
+                    {
+
+
+                    }
+                }
+            }
+        }
+    }
 }
