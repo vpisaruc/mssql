a566c59fb2899484a548e9a753b6aa213fea2f7c
vpisaruc <36450321+vpisaruc@users.noreply.github.com>
2018-11-22 12:52:27 +0300

    lab 4 begining

---
+++ lab4/CLR/CLR/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+
+public class HelloWorldProc
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void HelloWorld(out string text)
+    {
+        SqlContext.Pipe.Send("Hello world!" + Environment.NewLine);
+        text = "Hello world!";
+    }
+
+}
