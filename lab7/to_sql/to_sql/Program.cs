6f9951cb4d93bbae398df4aeaeec142c0d96828e
vpisaruc <36450321+vpisaruc@users.noreply.github.com>
2018-12-08 02:22:37 +0300

    prefinal version

---
+++ lab7/to_sql/to_sql/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace to_sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            linqClassesDataContext supermarket = new linqClassesDataContext();
+
+            var list = supermarket.GetTable<tbClient>();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.clientName);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ lab7/to_xml/to_xml/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace to_xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadXML();
+            Console.Write("\n\n\n");
+            ReadFirstXML();
+            Console.Write("\n\n\n");
+            UpdateXML();
+            Console.Write("\n\n\n");
+            WriteXML();
+            Console.ReadLine();
+        }
+
+        // ПРИМЕР 1. ЧТЕНИЕ ДАННЫХ ИЗ ДОКУМЕНТА XML
+        static void ReadXML()
+        {
+            XDocument xdoc = XDocument.Load(@"D:/university/database/mssql/lab6/task.xml");
+            var query = from people in xdoc.Descendants("Client")
+                        select people.Value;
+            Console.WriteLine("Найдено  {0} персонажей", query.Count());
+            Console.WriteLine();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        // ПРИМЕР 2. ЧТЕНИЕ ОДНОГО ЭЛЕМЕНТА ИЗ ДОКУМЕНТА XML
+        static void ReadFirstXML()
+        {
+            XDocument xdoc = XDocument.Load(@"D:/university/database/mssql/lab6/task.xml");
+            Console.WriteLine(xdoc.Element("gmailUsers").Element("Client").Element("clientName").Value);
+        }
+        
+        // ПРИМЕР 3. МОДИФИКАЦИЯ ДАННЫХ В ДОКУМЕНТЕ XML
+        static void UpdateXML()
+        {
+            XDocument xdoc = XDocument.Load(@"D:/university/database/mssql/lab6/task.xml");
+            xdoc.Element("gmailUsers").Element("Client").Element("clientTelephoneNumber").SetValue("1237847234");
+            Console.WriteLine(xdoc.Element("gmailUsers").Element("Client").Element("clientTelephoneNumber").Value);
+        }
+
+        // ПРИМЕР 4. ЗАПИСЬ ДАННЫХ В ДОКУМЕНТ XML
+        static void WriteXML()
+        {
+            XDocument xdoc = XDocument.Load(@"D:/university/database/mssql/lab6/task.xml");
+            XElement xe = new XElement("Client", "Нечто");
+            xdoc.Element("gmailUsers").Add(xe);
+            var query = from people in xdoc.Descendants("Client")
+                        select people.Value;
+            Console.WriteLine("Найдено {0} персонажей", query.Count());
+            Console.WriteLine();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
